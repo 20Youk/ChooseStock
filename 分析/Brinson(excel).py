45c0397ac53b8856ad7c9bff850564c223ba91c1
20Youk <30305851+20Youk@users.noreply.github.com>
2017-09-13 13:54:42 +0800

    增加代码收益明细

--- 分析/Brinson(excel).py
+++ 分析/Brinson(excel).py
@@ -32,7 +32,8 @@ def operatesheet(num):
     industrylist = []
     for ii in range(0, len(codelist)):
         industrylist.append(industrydict[codelist[ii]])
-    dict1 = {'datelist': datelist, 'codelist': codelist, 'industrylist': industrylist, 'mvlist': mvlist, 'buylist': buylist, 'netbuylist': netbuylist}
+    dict1 = {'datelist': datelist, 'codelist': codelist, 'industrylist': industrylist, 'mvlist': mvlist,
+             'buylist': buylist, 'netbuylist': netbuylist}
     dict2 = {'datelist': datelist, 'mvlist': mvlist}
     dataframe1 = pd.DataFrame(dict1, dtype=float)
     dataframe2 = pd.DataFrame(dict2, dtype=float)
@@ -46,16 +47,19 @@ def operatesheet(num):
         summvdict[ktem] = summvlist['mvlist'][ktem]
     # adjdict = {(date, code, industry): [mv, buy, netbuy]}
     for ntem in adjlist['mvlist'].keys():
-        adjdict[ntem[0], ntem[1], ntem[2]] = [adjlist['mvlist'][ntem], adjlist['buylist'][ntem], adjlist['netbuylist'][ntem]]
+        adjdict[ntem[0], ntem[1], ntem[2]] = [adjlist['mvlist'][ntem], adjlist['buylist'][ntem],
+                                              adjlist['netbuylist'][ntem]]
     # 计算每只股票收益率和权重(取前一日的权重和当日的收益率)
-    resultdict = {'date': [], 'industry': [], 'oneReturn': [], 'weight': []}
+    resultdict = {'date': [], 'code': [], 'industry': [], 'oneReturn': [], 'weight': []}
     for kk in range(1, len(tdate)):
         for jj in adjdict.keys():
             if jj[0] == tdate[kk] and adjdict.has_key((tdate[kk - 1], jj[1], jj[2])):
                 resultdict['date'].append(jj[0])
+                resultdict['code'].append(jj[1])
                 resultdict['industry'].append(jj[2])
                 jl = (tdate[kk - 1], jj[1], jj[2])
-                resultdict['oneReturn'].append((adjdict[jj][0] - adjdict[jl][0] - adjdict[jj][2]) / (adjdict[jl][0] + adjdict[jj][1]))
+                resultdict['oneReturn'].append(
+                    (adjdict[jj][0] - adjdict[jl][0] - adjdict[jj][2]) / (adjdict[jl][0] + adjdict[jj][1]))
                 resultdict['weight'].append(adjdict[jl][0] / summvdict[jl[0]])
     resultframe = pd.DataFrame(resultdict)
     resultframe['codeReturn'] = resultframe['oneReturn'] * resultframe['weight']
@@ -70,7 +74,7 @@ def operatesheet(num):
     # 计算基金每日整体收益
     datereturn = resultframe['codeReturn'].groupby(resultframe['date']).sum()
     datereturnlist = [[datereturn.keys()[ll], datereturn[ll]] for ll in range(0, len(datereturn))]
-    return result, sheetname, datereturnlist, adjresultdict
+    return result, sheetname, datereturnlist, adjresultdict, resultdict
 
 
 def readexcel(readfilepath, choosenum):
@@ -87,8 +91,10 @@ def readexcel(readfilepath, choosenum):
     else:
         valuelist3 = worksheet.col_values(3, start_rowx=1)
         valuelist4 = worksheet.col_values(4, start_rowx=1)
-        valuedict = [[valuelist0[ii], valuelist1[ii], valuelist2[ii], valuelist3[ii], valuelist4[ii]] for ii in range(0, len(valuelist1))]
+        valuedict = [[valuelist0[ii], valuelist1[ii], valuelist2[ii], valuelist3[ii], valuelist4[ii]] for ii in
+                     range(0, len(valuelist1))]
     return valuedict
+
 
 if __name__ == '__main__':
     sheetNum = input('input the num of sheet(larger than zero): ')
@@ -101,6 +107,7 @@ if __name__ == '__main__':
     writewb1 = localPath + 'GroupAnalysis_%s.xlsx' % today
     writewb2 = localPath + 'AllDateReturn_%s.xlsx' % today
     writewb3 = localPath + 'Brinson_%s.xlsx' % today
+    writewb4 = localPath + 'CodeReturn_%s.xlsx' % today
     # 字典{Num: IndexCode}
     fundStandard = readexcel(readFilePath1, 2)
     # 字典{(date, IndexCode): IndexReturn}
@@ -112,14 +119,16 @@ if __name__ == '__main__':
     wb1 = xlsxwriter.Workbook(writewb1)
     wb2 = xlsxwriter.Workbook(writewb2)
     wb3 = xlsxwriter.Workbook(writewb3)
+    wb4 = xlsxwriter.Workbook(writewb4)
     field1 = [u'日期', u'行业', u'收益', u'权重', u'调整因子']
     field2 = [u'日期', u'基金收益', u'基准收益', u'调整因子', u'基金复合收益率', u'基准复合收益率']
     field3 = [u'日期', u'基准代码', u'基准行业', u'基准行业收益', u'基准行业权重', u'基金行业收益', u'基金行业权重', 'AR', 'SR', 'RAAK', 'RSSK']
+    field4 = [u'日期', u'证券代码', u'行业', u'收益', u'权重']
     for k in range(0, sheetNum):
         # result1 = [[date, industry, return, weight]];
         # dayReturnList = [[date, fundReturn, indexReturn, kValue, RPK, RBK]]
         # resultDict1 = {(date, industry): [return, weight]}
-        result1, sheetName, dayReturnList, resultDict1 = operatesheet(k)
+        result1, sheetName, dayReturnList, resultDict1, codeReturn = operatesheet(k)
         indexCode = fundStandard[k]
         dayReturnList.sort(key=lambda x: x[0])
         dayList = [dtem[0] for dtem in dayReturnList]
@@ -147,7 +156,8 @@ if __name__ == '__main__':
             jtem.append(kValueDict[jtem[0]])
         # 计算积极资产配置组合的k期复合收益率RAAK，积极股票选择组合的k期复合收益率RSSK
         # 以基准行业为准，计算得出以下字典fundIndexDict = {日期， 基准代码， 基准行业， 基准行业收益， 基准行业权重，基金行业收益， 基金行业权重， AR， SR, RAAK, RSSK}
-        fundIndexDict = {'Date': [], 'IndexCode': [], 'Industry': [], 'IndexReturn': [], 'IndexWeight': [], 'FundReturn': [], 'FundWeight': [], 'AR': [], 'SR': []}
+        fundIndexDict = {'Date': [], 'IndexCode': [], 'Industry': [], 'IndexReturn': [], 'IndexWeight': [],
+                         'FundReturn': [], 'FundWeight': [], 'AR': [], 'SR': []}
         for ltem in indexIndRe:
             if ltem[1] == indexCode and ltem[0] in dayList:
                 fundIndexDict['Date'].append(ltem[0])
@@ -172,8 +182,9 @@ if __name__ == '__main__':
         fundIndexDict['RAAK'] = []
         fundIndexDict['RSSK'] = []
         for i in range(0, len(dayFundIndex.index)):
-            raaRssDict[dayFundIndex.index[i]] = [reduce(lambda x, y: (x + 1) * (y + 1) - 1, dayFundIndex['AR'][: i + 1]),
-                                                 reduce(lambda x, y: (x + 1) * (y + 1) - 1, dayFundIndex['SR'][: i + 1])]
+            raaRssDict[dayFundIndex.index[i]] = [
+                reduce(lambda x, y: (x + 1) * (y + 1) - 1, dayFundIndex['AR'][: i + 1]),
+                reduce(lambda x, y: (x + 1) * (y + 1) - 1, dayFundIndex['SR'][: i + 1])]
         for atem in fundIndexDict['Date']:
             fundIndexDict['RAAK'].append(raaRssDict[atem][0])
             fundIndexDict['RSSK'].append(raaRssDict[atem][1])
@@ -185,13 +196,16 @@ if __name__ == '__main__':
         Q3 = sum(fundIndexFrame['IndexWeight'] * fundIndexFrame['FundReturn'] * (fundIndexFrame['RSSK'] + 1))
         Q4 = sum(map(lambda x: x[1] * (x[4] + 1), dayReturnList))
         Q1 = sum(map(lambda x: x[2] * (x[5] + 1), dayReturnList))
-        fundIndexKeys = ['Date', 'IndexCode', 'Industry', 'IndexReturn', 'IndexWeight', 'FundReturn', 'FundWeight', 'AR', 'SR', 'RAAK', 'RSSK']
+        fundIndexKeys = ['Date', 'IndexCode', 'Industry', 'IndexReturn', 'IndexWeight', 'FundReturn', 'FundWeight',
+                         'AR', 'SR', 'RAAK', 'RSSK']
         sheet1 = wb1.add_worksheet(sheetName)
         sheet2 = wb2.add_worksheet(sheetName)
         sheet3 = wb3.add_worksheet(sheetName)
+        sheet4 = wb4.add_worksheet(sheetName)
         sheet1.set_column('A:A', 11)
         sheet2.set_column('A:A', 11)
         sheet3.set_column('A:A', 11)
+        sheet4.set_column('A:A', 11)
         for i in range(0, len(field1)):
             sheet1.write(0, i, field1[i])
         for row in range(1, len(result1) + 1):
@@ -208,5 +222,12 @@ if __name__ == '__main__':
             sheet3.write_column(1, col, fundIndexDict[fundIndexKeys[col]])
         sheet3.write_column(1, 12, ['Q1', 'Q2', 'Q3', 'Q4'])
         sheet3.write_column(1, 13, [Q1, Q2, Q3, Q4])
+        sheet4.write_row(0, 0, field4)
+        codeReturnKeys = ['date', 'code', 'industry', 'oneReturn', 'weight']
+        for i in range(0, len(codeReturnKeys)):
+            sheet4.write_column(1, i, codeReturn[codeReturnKeys[i]])
     wb1.close()
+    wb2.close()
+    wb3.close()
+    wb4.close()
     print 'Done!!!'
